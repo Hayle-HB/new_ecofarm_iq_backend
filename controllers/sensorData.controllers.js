666fac30fbac471e65d89a9dbee6bcaaf3b02577
hayle-hb <haylemeskelhaylemariam@gmail.com>
2025-06-19 21:25:50 +0700

    m

--- controllers/sensorData.controllers.js
+++ controllers/sensorData.controllers.js
@@ -20,36 +20,25 @@ exports.addSensorData = (req, res) => {
     leafWetness,
   } = req.body;
 
-  if (
-    ph === undefined ||
-    humidity === undefined ||
-    temperature === undefined ||
-    uv === undefined ||
-    nitrogen === undefined ||
-    phosphorus === undefined ||
-    potassium === undefined ||
-    waterLevel === undefined
-  ) {
-    return res.status(400).json({ message: "Missing required sensor fields." });
-  }
-
+  // If a value is missing, set it to 'not readed'
   const newEntry = {
     timestamp: new Date().toISOString(),
-    ph,
-    humidity,
-    temperature,
-    uv,
-    nitrogen,
-    phosphorus,
-    potassium,
-    waterLevel,
-    co2,
-    soilMoisture,
-    lightIntensity,
-    ec,
-    rainfall,
-    windSpeed,
-    leafWetness,
+    ph: ph !== undefined ? ph : "not readed",
+    humidity: humidity !== undefined ? humidity : "not readed",
+    temperature: temperature !== undefined ? temperature : "not readed",
+    uv: uv !== undefined ? uv : "not readed",
+    nitrogen: nitrogen !== undefined ? nitrogen : "not readed",
+    phosphorus: phosphorus !== undefined ? phosphorus : "not readed",
+    potassium: potassium !== undefined ? potassium : "not readed",
+    waterLevel: waterLevel !== undefined ? waterLevel : "not readed",
+    co2: co2 !== undefined ? co2 : "not readed",
+    soilMoisture: soilMoisture !== undefined ? soilMoisture : "not readed",
+    lightIntensity:
+      lightIntensity !== undefined ? lightIntensity : "not readed",
+    ec: ec !== undefined ? ec : "not readed",
+    rainfall: rainfall !== undefined ? rainfall : "not readed",
+    windSpeed: windSpeed !== undefined ? windSpeed : "not readed",
+    leafWetness: leafWetness !== undefined ? leafWetness : "not readed",
   };
 
   let data = [];
@@ -60,9 +49,7 @@ exports.addSensorData = (req, res) => {
   }
   data.push(newEntry);
   sensorDataService.writeSensorData(data);
-  res
-    .status(201)
-    .send("Sensor data added successfully.");
+  res.status(201).send("Sensor data added successfully.");
 };
 
 // Controller to get all sensor data
